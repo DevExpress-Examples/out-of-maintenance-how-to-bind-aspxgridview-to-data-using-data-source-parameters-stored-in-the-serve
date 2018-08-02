b0e0791fc4300f218b54754e36005d2e27df5fe6
DevExpressExampleBot <fakeEmail>
2018-08-02 11:54:10 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx
+++ VB/WebSite/Default.aspx
@@ -1,24 +1,21 @@
-<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
-<%@ Register Assembly="DevExpress.Web.ASPxGridView.v9.1" Namespace="DevExpress.Web.ASPxGridView"
+<%@ Register Assembly="DevExpress.Web.v13.1" Namespace="DevExpress.Web.ASPxGridView"
     TagPrefix="dxwgv" %>
-<%@ Register Assembly="DevExpress.Web.ASPxEditors.v9.1" Namespace="DevExpress.Web.ASPxEditors"
+<%@ Register Assembly="DevExpress.Web.v13.1" Namespace="DevExpress.Web.ASPxEditors"
     TagPrefix="dxe" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>How to bind ASPxGridView to data using data source parameters stored in server
-        session</title>
+    <title>How to bind ASPxGridView to data using data source parameters stored in server session</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            <h2>
-                Data source without select parameters</h2>
+            <h2>Data source without select parameters</h2>
             <dxwgv:ASPxGridView ID="ASPxGridView1" runat="server" OnDataBinding="ASPxGridView1_DataBinding">
             </dxwgv:ASPxGridView>
-            <h2>
-                Data source with select parameters</h2>
+            <h2>Data source with select parameters</h2>
             <table>
                 <tr>
                     <td>
@@ -26,8 +23,8 @@
                         </dxe:ASPxLabel>
                     </td>
                     <td>
-                        <dxe:ASPxComboBox runat="server" ID="ASPxComboBox1" AutoPostBack="true" SelectedIndex="0"
-                            DataSourceID="AccessDataSource1" ValueType="System.String" ValueField="CategoryID"
+                        <dxe:ASPxComboBox runat="server" ID="ASPxComboBox1" AutoPostBack="true" SelectedIndex="0" 
+                            DataSourceID="AccessDataSource1" ValueType="System.String" ValueField="CategoryID" 
                             TextField="CategoryID" OnSelectedIndexChanged="ASPxComboBox1_SelectedIndexChanged">
                         </dxe:ASPxComboBox>
                     </td>
@@ -37,7 +34,8 @@
             </dxwgv:ASPxGridView>
         </div>
     </form>
-    <asp:AccessDataSource ID="AccessDataSource1" runat="server" DataFile="~/App_Data/nwind.mdb"
+
+    <asp:AccessDataSource ID="AccessDataSource1" runat="server" DataFile="~/App_Data/nwind.mdb" 
         SelectCommand="SELECT [CategoryID] FROM [Categories]"></asp:AccessDataSource>
     <asp:AccessDataSource ID="AccessDataSource2" runat="server" DataFile="~/App_Data/nwind.mdb"
         SelectCommand="SELECT [ProductID], [ProductName], [CategoryID], [UnitPrice], [QuantityPerUnit] FROM [Products]">
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Data
 Imports System.Configuration
 Imports System.Web
@@ -12,47 +11,48 @@ Imports System.Collections.Specialized
 Imports DevExpress.Web.ASPxGridView
 Imports DevExpress.Web.ASPxEditors
 
-Public Partial Class _Default
-	Inherits System.Web.UI.Page
-	Private Const SessionDataSource1 As String = "SessionDataSource1Key"
-	Private Const SessionDataSource2 As String = "SessionDataSource2Key"
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		If Session(SessionDataSource1) Is Nothing Then
-			Dim parameters1 As DataSourceParameters = New DataSourceParameters("~/App_Data/nwind.mdb", "SELECT [CategoryID], [CategoryName], [Description] FROM [Categories]")
-			Session(SessionDataSource1) = parameters1
-		End If
-		If Session(SessionDataSource2) Is Nothing Then
-			Dim parameters2 As DataSourceParameters = New DataSourceParameters("~/App_Data/nwind.mdb", "SELECT [ProductID], [ProductName], [CategoryID], [UnitPrice], [QuantityPerUnit] FROM [Products] WHERE [CategoryID] = @CategoryID")
-			parameters2.SelectParameters.Add("CategoryID", "1")
-			Session(SessionDataSource2) = parameters2
-		End If
-		ASPxGridView1.DataBind()
-		ASPxGridView2.DataBind()
-	End Sub
-	Protected Sub ASPxGridView1_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Private Const SessionDataSource1 As String = "SessionDataSource1Key"
+    Private Const SessionDataSource2 As String = "SessionDataSource2Key"
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        If Session(SessionDataSource1) Is Nothing Then
+            Dim parameters1 As New DataSourceParameters("~/App_Data/nwind.mdb", "SELECT [CategoryID], [CategoryName], [Description] FROM [Categories]")
+            Session(SessionDataSource1) = parameters1
+        End If
+        If Session(SessionDataSource2) Is Nothing Then
+            Dim parameters2 As New DataSourceParameters("~/App_Data/nwind.mdb", "SELECT [ProductID], [ProductName], [CategoryID], [UnitPrice], [QuantityPerUnit] FROM [Products] WHERE [CategoryID] = @CategoryID")
+            parameters2.SelectParameters.Add("CategoryID", "1")
+            Session(SessionDataSource2) = parameters2
+        End If
+        ASPxGridView1.DataBind()
+        ASPxGridView2.DataBind()
+    End Sub
+    Protected Sub ASPxGridView1_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
         TryCast(sender, ASPxGridView).DataSource = RestoreAccessDataSourceFormSessionParameters(SessionDataSource1)
-	End Sub
-	Protected Sub ASPxComboBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs)
-		Dim comboBox As ASPxComboBox = TryCast(sender, ASPxComboBox)
-		If comboBox.SelectedIndex = -1 Then
-		Return
-		End If
-		Dim parameters As DataSourceParameters = TryCast(Session(SessionDataSource2), DataSourceParameters)
-		parameters.SelectParameters("CategoryID") = comboBox.SelectedItem.Value.ToString()
-		ASPxGridView2.DataBind()
-	End Sub
-	Protected Sub ASPxGridView2_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub ASPxComboBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs)
+        Dim comboBox As ASPxComboBox = TryCast(sender, ASPxComboBox)
+        If comboBox.SelectedIndex = -1 Then
+            Return
+        End If
+        Dim parameters As DataSourceParameters = TryCast(Session(SessionDataSource2), DataSourceParameters)
+        parameters.SelectParameters("CategoryID") = comboBox.SelectedItem.Value.ToString()
+        ASPxGridView2.DataBind()
+    End Sub
+    Protected Sub ASPxGridView2_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
         TryCast(sender, ASPxGridView).DataSource = RestoreAccessDataSourceFormSessionParameters(SessionDataSource2)
-	End Sub
-	Protected Function RestoreAccessDataSourceFormSessionParameters(ByVal dataSourceParametersSessionKey As String) As AccessDataSource
-		Dim parameters As DataSourceParameters = TryCast(Session(dataSourceParametersSessionKey), DataSourceParameters)
-		If parameters Is Nothing Then
+    End Sub
+    Protected Function RestoreAccessDataSourceFormSessionParameters(ByVal dataSourceParametersSessionKey As String) As AccessDataSource
+        Dim parameters As DataSourceParameters = TryCast(Session(dataSourceParametersSessionKey), DataSourceParameters)
+        If parameters Is Nothing Then
             Return Nothing
-		End If
-		Dim dataSource As AccessDataSource = New AccessDataSource(parameters.DataFile, parameters.SelectComand)
-		For Each key As String In parameters.SelectParameters.Keys
-			dataSource.SelectParameters.Add(key, parameters.SelectParameters(key))
-		Next key
-		Return dataSource
-	End Function
+        End If
+        Dim dataSource As New AccessDataSource(parameters.DataFile, parameters.SelectComand)
+        For Each key As String In parameters.SelectParameters.Keys
+            dataSource.SelectParameters.Add(key, parameters.SelectParameters(key))
+        Next key
+        Return dataSource
+    End Function
 End Class
